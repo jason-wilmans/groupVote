b3d10dffa13dbc030a3b9d62bd2f2f85a2ee5b27
causeNo <jason.wilmans@haw-hamburg.de>
2018-07-27 20:56:20 +0200

    Added Tournament and match making algorithm (server side)

--- code/server/src/github.com/jason-wilmans/votesComponent/domainObjects/Match.go
+++ code/server/src/github.com/jason-wilmans/votesComponent/domainObjects/Match.go
@@ -1,7 +1,39 @@
 package domainObjects
 
+import (
+	"github.com/satori/go.uuid"
+	"github.com/muroc/prego"
+)
+
 type Match struct {
 	Id				string
 	CompetitorAId	string
 	CompetitorBId	string
+	Started			bool
+	Finished		bool
+}
+
+func NewMatch() *Match {
+	id, err := uuid.NewV4()
+	if err != nil {
+		panic(err)
+	}
+
+	return &Match{id.String(), "", "", false, false}
+}
+
+func (this *Match) HasRoom() bool {
+	return this.CompetitorAId == "" || this.CompetitorBId == ""
+}
+
+func (this *Match) Join(competitorId uuid.UUID) {
+	precond.True(this.HasRoom(), "")
+	precond.False(this.Started || this.Finished, "")
+
+	if this.CompetitorAId == "" {
+		this.CompetitorAId = competitorId.String()
+		return
+	}
+
+	this.CompetitorBId = competitorId.String()
 }
--- code/server/src/github.com/jason-wilmans/votesComponent/useCases/TournamentUC.go
+++ code/server/src/github.com/jason-wilmans/votesComponent/useCases/TournamentUC.go
@@ -3,20 +3,60 @@ package useCases
 import (
 	"github.com/satori/go.uuid"
 	"github.com/jason-wilmans/votesComponent/domainObjects"
+	"time"
+	"math/rand"
 )
 
 type TournamentUC struct {
 	tournaments		map[uuid.UUID]*domainObjects.Tournament
 	optionUC		*OptionUC
+	random			*rand.Rand
 }
 
 func NewTournamentUC(optionUC *OptionUC) *TournamentUC {
 	tournaments := make(map[uuid.UUID]*domainObjects.Tournament)
-	return &TournamentUC{tournaments, optionUC}
+	seed := time.Now().UnixNano()
+	random := rand.New(rand.NewSource(seed))
+	return &TournamentUC{tournaments, optionUC, random}
 }
 
-func (this *TournamentUC) StartTournament(templateId uuid.UUID) {
-	matches := make([]*domainObjects.Match, 0)
+func (this *TournamentUC) StartTournament(templateId uuid.UUID) *domainObjects.Tournament {
+	options	:= this.optionUC.GetAllForTemplateId(templateId)
+	matches := createInitialMatches(options, this.random)
 	tournament := domainObjects.NewTournament(matches)
 	this.tournaments[tournament.GetId()] = tournament
+	return tournament
+}
+
+func createInitialMatches(options []*domainObjects.Option, random *rand.Rand) []*domainObjects.Match {
+	nrOfOptions := len(options)
+	nrOfMatches := calculateNumberOfMatches(nrOfOptions)
+
+	matches := make([]*domainObjects.Match, 0)
+	for i := 0; i < nrOfMatches; i++ {
+		match := domainObjects.NewMatch()
+		matches = append(matches, match)
+	}
+
+	for _, option := range options {
+		placeOptionInRandomMatch(option, matches, random)
+	}
+	return matches
+}
+
+func calculateNumberOfMatches(numberOfOptions int) int {
+	numberOfMatches := 1
+	for numberOfMatches < numberOfOptions {
+		numberOfMatches = numberOfMatches * 2
+	}
+	return numberOfMatches
+}
+
+func placeOptionInRandomMatch(option *domainObjects.Option, matches []*domainObjects.Match, random *rand.Rand) {
+	nrOfMatches := len(matches)
+	randomIndex := random.Intn(nrOfMatches)
+	for !matches[randomIndex].HasRoom() {
+		randomIndex = random.Intn(nrOfMatches)
+	}
+	matches[randomIndex].Join(option.GetId())
 }
